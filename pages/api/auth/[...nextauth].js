387d5fd34a2120e61bf3118fd001399dd085ce8d
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-14 16:14:56 +0900

    Maria db Board 클래스 구현 5 - nextAuth 2 - 인증 절차 추가

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,22 +1,26 @@
 // npm install next-auth@3.29.10 --save-dev
-// 경로 : /page/api/auth/[...nextauth].js
+// 경로 : /pages/api/auth/[...nextauth].js
 
-import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials'
+import NextAuth from "next-auth";
+import Credentials from 'next-auth/providers/credentials';
 
 export default NextAuth({
     providers: [
         Credentials({
-            name:"email-passwd-credentials",
+            name: "email-passwd-credentials",
             credentials: {
-                username: {label:"이메일", placeholder: 'email'},
-                password: {label:"비밀번호", type:"password"}
+                email: { label: "이메일", type: "email" },
+                passwd: { label: "비밀번호", type: "password" }
             }, // 로그인 폼 정의
             async authorize(credentials, req) {
-                console.log('auth login -', credentials)
+                // 입력한 인증 정보 가져옴
+                const email = credentials.email;
+                const passwd = credentials.passwd;
+
+                // 인증에 성공해야만 로그인 허용
+                if (email === 'abc123@987xyz.com' && passwd === '987xyz')
                 return credentials;
             }
         })
     ]
-})
-
+});
