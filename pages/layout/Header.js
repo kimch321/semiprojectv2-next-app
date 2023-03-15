544ec82288711048f59c4935f5d831130b69f32e
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-15 10:55:48 +0900

    Maria db Board 클래스 구현 5 - nextAuth 6 - header 로그인/로그아웃

--- pages/layout/Header.js
+++ pages/layout/Header.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import {getSession} from "next-auth/client";
 
 // Link로 해두면 프리 렌더링 된다.
+// component에는 severSideprops등 사용 불가능.
 
-const Header = () => {
+const Header = ({menu}) => {
+    console.log('header1 -', menu)
     return(
         <>
             <header><h1>NextJS 프로젝트 v1</h1></header>
@@ -10,7 +13,10 @@ const Header = () => {
                 <ul>
                     <li><Link href="/">Home</Link></li>
                     <li><a href="/member/join">회원가입</a></li>
-                    <li><a href="/member/login">로그인</a></li>
+
+                    {/* 문자열을 html 태그로 출력 - dangerouslySetInnerHTML */}
+                    <li dangerouslySetInnerHTML={{ __html : menu }}></li>
+
                     <li><Link href="/board/list">게시판</Link></li>
                     <li><Link href="/member/myinfo">회원정보</Link></li>
                 </ul>
--- pages/layout/Layout.js
+++ pages/layout/Layout.js
@@ -3,10 +3,11 @@ import Footer from "./Footer";
 
 // localhost:3000/member/join (join 부분을 엔드포인트라고 한다. 이 부분이 children에 할당되게 횐다.)
 
-const Layout = ({children}) => {
+const Layout = ({children, menu}) => {
+    console.log('layout -',menu)
     return(
         <div id="wrapper">
-            <Header />
+            <Header menu={menu} />
             {children}
             <Footer />
         </div>
